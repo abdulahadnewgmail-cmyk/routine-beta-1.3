74e1df836c705a9839cea3f5af6cd738f94898a9
abdulahadnewgmail-cmyk <abdul.ahad.new.gmail@gmail.com>
2026-03-16 14:28:57 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,56 @@
-const CACHE_NAME = "routine-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./table.html",
-  "./header.css",
-  "./routine.css",
-  "./table.css",
-  "./r.js",
-  "./data_r.js",
-  "./table.js",
-  "./practice.js",
-  "./manifest.json"
+const cacheName = 'somoy-routine-v1.3';
+const assets = [
+  './',
+  './index.html',
+  './table.html',
+  './routine.css',
+  './header.css',
+  './table.css',
+  './r.js',
+  './data_r.js',
+  './table.js',
+  './manifest.json' // Recommended to add this
 ];
 
-// Install Service Worker and cache files
-self.addEventListener("install", (event) => {
+// Install: Save everything to the phone
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
     })
   );
+  self.skipWaiting();
 });
 
-// Activate and clean up old caches
-self.addEventListener("activate", (event) => {
+// Activate: Delete old versions of the app
+self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
       );
     })
   );
 });
 
-// Fetching strategy: Cache first, then Network
-self.addEventListener("fetch", (event) => {
+// Fetch: Serve from cache but update in the background
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        const fetchPromise = fetch(event.request).then((networkResponse) => {
+          // Update the cache with the new version from the internet
+          if (networkResponse.status === 200) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }).catch(() => {
+          // If totally offline, this catch handles the error silently
+        });
+        
+        // Return the cached version immediately, or wait for network if not in cache
+        return response || fetchPromise;
+      });
     })
   );
 });
