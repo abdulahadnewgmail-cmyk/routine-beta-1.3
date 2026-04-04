62f3a02fbf007a4d6c56f835c1b04d4eb23fad5b
abdulahadnewgmail-cmyk <abdul.ahad.new.gmail@gmail.com>
2026-04-04 21:41:14 +0600

    Update table.js 1.5

--- table.js
+++ table.js
@@ -1,94 +1,123 @@
-  let tableContainer = document.querySelector('.table_container')
-  let savedRoutine;
+let routine;
   
-  function getTableData() {
-    let savedData = JSON.parse(localStorage.getItem('savedRoutine'))
-    savedRoutine = savedData || prevRoutine
-
+  function getUserGroup() {
+    let userRoutine;
+    let usergroup3 = localStorage.getItem('user')
+    let usergroup2 = JSON.parse(usergroup3)
+    let usergroup = usergroup2.group
+    
+    if (usergroup === 'SCIENCE') {
+      userRoutine = SCIENCE;
+    }
+    else if (usergroup === 'ARTS') {
+      userRoutine = ARTS;
+    }
+    else if (usergroup === 'COMMERCE') {
+      userRoutine = COMMERCE;
+    }
+    else{userRoutine = SCIENCE;}
+    return userRoutine;
   }
   
-  function createTable(array) {
-    let html;
-    let rows = '';
-    let th = ''
-    for (var i = 510; i <= 790; i += 40) {
-      th += `
-        <th class='a${i}'> 
-          ${i >= 550? convertToHM(i): ''}
-        </th>
-      `
+  function declareRoutine() {
+    routine = JSON.parse(localStorage.getItem('savedRoutine')) || SCIENCE
+  }
+  
+  function createTable(current_routine_array) {
+    
+    function minToHm(minute_format_time) {
+      let hr = String(Math.floor((minute_format_time / 60) % 12))
+      if(Number(hr) < 1) hr = String(Number(hr) + 12);
+      let min = String(minute_format_time % 60)
+       
+      return `${hr.padStart(2, '0')} : ${min.padStart(2, '0')}`
     }
     
-    for (let i = 0; i <= array.length - 1; i++) {
-      
-      let object = array[i]
-      let index = 550;
-      let cells = `
-      `;
-      
-      while (index <= 790) {
-        cells += `
-        <td>
-          <input value='${object[index]}'>
-        </td>
-        `
-        index += 40
-      }
-      rows += `
-        <tr>
-          <td class='tableDayName'>
-            ${days[i]}
-          </td>
-          ${cells}
-        </tr>
-      `
+    function getWeekday() {
+      const date = new Date()
+      weekDay = date.getDay()
+      return weekDay;
     }
-    html = `
-    <table>
-      ${th}
-      ${rows}
-    </table>
-    `
-    tableContainer.innerHTML = html
-  }
-  
-  function convertToHM(totalMinutes) {
-    let h = Math.floor(totalMinutes / 60).toString().padStart(2, '0')
-    h = Number(h) < 13? h : (h -12).toString().padStart(2, '0')
-    let m = (totalMinutes % 60).toString().padStart(2, '0')
-    return `${h} : ${m}`
+    
+    function createAllRows() {
+      function createHeaderCells() {
+        let headerCells = '<th class="cornerCell"></th>';
+        for (let i = 550; i <= 830; i += 40) {
+          headerCells += `
+                  <th>${minToHm(i)}</th>
+                `
+        }
+        return headerCells;
+      }
+      function createRows() {
+        
+        let rows = `<tr>${headerCells}</tr>`
+        for (var index = 0; index < current_routine_array.length; index++) {
+          let name = String(days[index])
+          let value = name.slice(0,name.length - 3)
+          let cells = ` <td class='tableDayNames'>
+            ${value}
+            </td>`
+            
+          for (let i = 550; i <= 830; i += 40) {
+            cells += `
+              <td><input value="${current_routine_array[index][i]}"></td>
+            `
+          }
+          rows += `<tr>
+            ${cells}
+          </tr>`
+        }
+        return rows;
+      }
+      
+      let headerCells = createHeaderCells()
+      let allRows = createRows()
+      return allRows;
+    }
+    
+    function updateUi() {
+      const cont = document.querySelector('.tableContainer');
+      cont.innerHTML = `<table class='tableContainer'>
+        ${allrows}
+      </table>`
+      return 'done';
+    }
+    
+    let weekday = getWeekday()
+    let allrows = createAllRows()
+    let updateui = updateUi()
   }
   
   function saveRoutine() {
-    let allRows = document.querySelectorAll('tr')
+    const tbody = document.querySelector('tbody')
+    const tbodyC = tbody.childElementCount
     
-    for (let i = 1; i < allRows.length; i++) {
-      let object = routine[i-1]
-      let eachRow = allRows[i]
-      let index = 550;
-      for (let j = 1; j < eachRow.childElementCount; j++) {
-        let eachCulumn = eachRow.children[j].children[0]
-        object[index] = eachCulumn.value ;
-        index += 40
+    for (var i = 1; i < tbodyC; i++) {
+      const selectedRow = tbody.children[i]
+      const selectedObject = routine[i - 1]
+      
+      const selectedRowC = selectedRow.childElementCount;
+      let selectedKey = 550;
+      for (var j = 1; j < selectedRowC; j++) {
+        const selectedCellValue = selectedRow.children[j].children[0].value || 'নাই';
+        selectedObject[selectedKey] = selectedCellValue;
+        selectedKey += 40;
       }
     }
-    savedRoutine = routine
-    
-    localStorage.setItem('savedRoutine', JSON.stringify(savedRoutine))
-    let parsed = JSON.parse(localStorage.getItem('savedRoutine'))
-    createTable(parsed)
-    window.alert(' সফল')
+    alert('ROUTINE SAVED')
+    createTable(routine)
+    localStorage.setItem('savedRoutine', JSON.stringify(routine))
   }
   
   function resetRoutine() {
-    let ans = confirm(' সকল রুটিন রিসেট হয়ে যাবে !')
-    ans && localStorage.removeItem('savedRoutine')
-    ans && createTable(prevRoutine)
+    if(!confirm(' তুমি কি সমস্ত রুটিন রিসেট করতে চাও ?')) return ;
+    localStorage.setItem('savedRoutine', JSON.stringify(getUserGroup()))
+    createTable(getUserGroup())
+    
   }
-  
-  getTableData()
-  createTable(savedRoutine)
-  
-  
-  
-  
+
+
+  declareRoutine()
+  createTable(routine)
+  updateUserName()
